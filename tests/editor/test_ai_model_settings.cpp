ba7b7c55b5f6cb2c9fed1b8e487e9d69c9be6f73
Fantety <fantety@outlook.com>
2026-06-21 14:02:59 +0800

    Fix static check formatting issues

--- tests/editor/test_ai_model_settings.cpp
+++ tests/editor/test_ai_model_settings.cpp
@@ -1,23 +1,48 @@
 /**************************************************************************/
 /*  test_ai_model_settings.cpp                                            */
 /**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
 
-#include "tests/test_macros.h"
-
-#include "editor/agent_v1/config/ai_config_service.h"
-#include "editor/agent_v1/tools/editor/ai_change_set_store.h"
-#include "editor/agent_v1/ui_adapter/ai_agent_v1_ui_bridge.h"
+#include "core/io/dir_access.h"
+#include "core/io/file_access.h"
+#include "core/version.h"
 #include "editor/agent_ui/ai_composer.h"
 #include "editor/agent_ui/ai_settings_about_page.h"
 #include "editor/agent_ui/ai_settings_mcp_page.h"
 #include "editor/agent_ui/ai_settings_models_page.h"
 #include "editor/agent_ui/ai_settings_rules_page.h"
 #include "editor/agent_ui/ai_settings_skills_page.h"
+#include "editor/agent_v1/config/ai_config_service.h"
+#include "editor/agent_v1/tools/editor/ai_change_set_store.h"
+#include "editor/agent_v1/ui_adapter/ai_agent_v1_ui_bridge.h"
 #include "editor/next_engine_update_checker.h"
-#include "core/version.h"
-#include "core/io/dir_access.h"
-#include "core/io/file_access.h"
 #include "scene/main/http_request.h"
+#include "tests/test_macros.h"
 
 TEST_FORCE_LINK(test_ai_model_settings);
 
